2fc0ba4a1487c86d31a7282b7ea26d3e95351cba
Danial Hussain <dhgilani@gmail.com>
2018-07-05 00:39:40 +0500

    root path to css files

--- stapp/settings.py
+++ stapp/settings.py
@@ -122,7 +122,7 @@ AUTH_USER_MODEL = 'MyApp.Teacher'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+STATIC_ROOT = '/home/dhgillani/MoreToLearn/static'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
